9a4de99e6d550b23c4fc35856e564678dc58cf36
smdp2000 <somyadeep99@gmail.com>
2023-11-16 08:21:05 -0800

    gradient-colorfix

--- src/HeatMap.jsx
+++ src/HeatMap.jsx
@@ -1,11 +1,19 @@
-import { Table, Container, Row, Col, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Table, Container, Row, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import './App.css';
 
 const HeatmapTable = ({ data = [], shapeletNames = [], modelNames = [] }) => {
     const getColor = value => {
-        const normalized = (value + 1) / 2;
-        const intensity = Math.round(255 * (1 - normalized));
-        return `rgb(255, ${intensity}, ${intensity})`;
+        if (value === 0) {
+            return '#ffffff'; // white for neutral/zero
+        } else if (value < 0) {
+            // Blue for negative
+            const intensity = Math.round(255 * (1 + value)); // Lighter to full blue for -1 to 0
+            return `rgb(${intensity}, ${intensity}, 255)`;
+        } else {
+            // Red for positive
+            const intensity = Math.round(255 * (1 - value)); // Full to lighter red for 0 to 1
+            return `rgb(255, ${intensity}, ${intensity})`;
+        }
     };
 
     const renderTooltip = (props, value) => (
@@ -15,10 +23,16 @@ const HeatmapTable = ({ data = [], shapeletNames = [], modelNames = [] }) => {
     );
 
     const gradientBarStyle = {
-        background: "linear-gradient(to bottom, #ffffff 0%, rgb(255, 0, 0) 100%)",
+        background: "linear-gradient(to bottom, rgb(0, 0, 255) 0%, #ffffff 50%, rgb(255, 0, 0) 100%)",
         height: "100px",
         width: "20px",
-        margin: "0 10px"
+        margin: "0 10px",
+    };
+
+    const labelStyle = {
+        position: "absolute", 
+        left: "-14px", // Position the labels to the left of the bar
+        fontSize: "0.8rem", // Adjust font size as needed
     };
 
     const images = ['/Inc.png', '/peak.png', '/dec.png', '/flat.png'];
@@ -27,14 +41,14 @@ const HeatmapTable = ({ data = [], shapeletNames = [], modelNames = [] }) => {
         <Container>
             <Row className="justify-content-md-center">
                 <div style={{ display: "flex", width: "100%", overflowX: "auto" }}>
-                    <div style={{ flexGrow: 1, maxWidth: "calc(100% - 50px)" }}>
+                    <div style={{ flexGrow: 1 }}>
                         <Table responsive="md" className="heatmap-table">
                             <thead>
                                 <tr>
                                     <th></th>
                                     {images.map((src, index) => (
                                         <th key={index} style={{ textAlign: 'center' }}>
-                                            <Image src={process.env.PUBLIC_URL+src} alt={`Image ${index}`} thumbnail width={100} />
+                                            <Image src={process.env.PUBLIC_URL + src} alt={`Image ${index}`} thumbnail width={100} />
                                         </th>
                                     ))}
                                 </tr>
@@ -65,12 +79,12 @@ const HeatmapTable = ({ data = [], shapeletNames = [], modelNames = [] }) => {
                             </tbody>
                         </Table>
                     </div>
-                    <div style={{ width: "50px", flexShrink: 0 }}>
+                    <div style={{ flexShrink: 0 }}>
                         <div className="gradient-bar" style={gradientBarStyle}>
                             <div style={{ position: "relative", height: "100%" }}>
-                                <span style={{ position: "absolute", top: 0, right: "25px" }}>-1</span>
-                                <span style={{ position: "absolute", top: "45%", right: "25px" }}>0</span>
-                                <span style={{ position: "absolute", bottom: 0, right: "25px" }}>1</span>
+                                <span style={{ ...labelStyle, top: 0 }}>-1</span>
+                                <span style={{ ...labelStyle, top: "45%" }}>0</span>
+                                <span style={{ ...labelStyle, bottom: 0 }}>1</span>
                             </div>
                         </div>
                     </div>
